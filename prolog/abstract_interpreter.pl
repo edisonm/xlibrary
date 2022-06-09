2a3cde92f09d5205d86be45c9c0d5881c5d048c6
Edison Mera <mera@process-design-center.com>
2022-06-09 16:31:59 +0200

    Fixed evaluable_body_hook for clause/{2,3} predicates

--- prolog/abstract_interpreter.pl
+++ prolog/abstract_interpreter.pl
@@ -130,8 +130,8 @@ evaluable_body_hook(is_list(A), _, (ground(A);is_list(A))).
 evaluable_body_hook(number(A),  _, nonvar(A)).
 evaluable_body_hook(float(A),   _, nonvar(A)).
 evaluable_body_hook(integer(A), _, nonvar(A)).
-evaluable_body_hook(clause(A, _), _, nonvar(A)).
-evaluable_body_hook(clause(A, _, _), _, nonvar(A)).
+evaluable_body_hook(clause(A, _),    _, (strip_module(A, M, B), atom(M), callable(B))).
+evaluable_body_hook(clause(A, _, _), _, (strip_module(A, M, B), atom(M), callable(B))).
 evaluable_body_hook(format(Out, Format, Args), _,
                     (compound(Out), nonvar(Format), ground(Args))).
 evaluable_body_hook(sort(A, _), _, (is_list(A), maplist(nonvar, A))).
